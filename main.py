358104f8bb43eed91f41990e808cebce94ae0d19
Andrey <and-lukyane@yandex.ru>
2019-04-19 07:52:48 +0300

    Update main.py
    Debugging

--- main.py
+++ main.py
@@ -31,6 +31,10 @@ def get_data():
     if request.method == 'POST':
         data_dict = data_manager.get_initial_data()
         print('data_dict', data_dict.keys())
+        for k, v in data_dict.items():
+            print(k)
+            print(v)
+            print('-'*50)
         # data_dict = {'rubrics_dict': rubrics_dict, 'topics_dict': topics_dict}
         return json.dumps(data_dict)
 
